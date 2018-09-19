6ed2e04652bc1a9ebdbaebdd13e35b5a7d120816
i.muhin <i.muhin@cdek.ru>
2018-09-19 22:31:03 +0300

    params у гет запросов

--- backend/modules/autocomplete.js
+++ backend/modules/autocomplete.js
@@ -5,13 +5,16 @@ module.exports.initAuthApi = (app) => {
 // get: /autocomplete/anime?name=хвост
 // resp: status 2** { anime: [<anime>, ...] }
     app.get('/autocomplete/anime', (req, res) => {
+        let name = req.query.name || '';
 
-        res.send('123');
+        res.send('123 ' + name);
     });
 
 // get: /autocomplete/genre?name=фэ
 // resp: status 2** { genre: [<genre>, ...] }
     app.get('/autocomplete/genre', (req, res) => {
+        let name = req.query.name || '';
+
         res.send('123');
     });
 
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const query = require('url');
+const url = require('url');
+const querystring = require('querystring');
 
 const auth = require('./modules/auth');
 const anime = require('./modules/anime');
@@ -12,7 +13,8 @@ const autocomplete = require('./modules/autocomplete');
 const app  = express();
 const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 auth.initAuthApi(app);
 anime.initAuthApi(app);
