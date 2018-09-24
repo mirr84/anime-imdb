0514ed7f7729e336b6230eafb6cfa2b9b6b8d497
ilya <mirr84@mail.ru>
2018-09-24 14:13:52 +0300

    precommit

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,13 @@ const db_config = {
     database: 'y913929d_anime'
 }
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
