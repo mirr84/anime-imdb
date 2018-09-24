ec4178564a2ed80092d63076e223c76b8f0c37bb
ilya <mirr84@mail.ru>
2018-09-24 14:28:29 +0300

    precommit

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,12 @@ const db_config = {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-app.use(cors({
-  'allowedHeaders': ['token', 'Content-Type'],
-  'exposedHeaders': ['token'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 auth.initAuthApi(app, mysql, db_config);
 anime.initAuthApi(app, mysql, db_config);
