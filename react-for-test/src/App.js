b77553e8c533f2f9ec2b67201f31ff769aa8c8cf
ilya <mirr84@mail.ru>
2018-09-24 15:39:31 +0300

    precommit

--- react-for-test/src/App.js
+++ react-for-test/src/App.js
@@ -32,50 +32,49 @@ class App extends Component {
     super(props);
     this.toggle = this.toggle.bind(this);
     this.selectSection = this.selectSection.bind(this);
-    this.checkAuth = this.checkAuth.bind(this);
-    this.state = { isOpen: false };
+    this.state = { isOpen: false, menu: {} };
   }
 
   toggle() { this.setState({ isOpen: !this.state.isOpen }); }  
   selectSection (section) { localStorage.section = section; }
 
-	 myFunc() {
-	 	this.refs.notify.notificationAlert(
-	 		{
-			    place: 'br',
-			    message: (
-			        <div>
-			            <div>
-			                Welcome to Now UI Dashboard React
-			            </div>
-			        </div>
-			    ),
-			    type: "info",
-			    icon: "",
-			    autoDismiss: 5
-			}
-	 	);
-	 }
-
-	 checkAuth = () => {
-	 	this.myFunc();
-	 }
-
   componentDidMount() {
+
+    let headers = localStorage.token ? { 'sessionId': localStorage.token } : '';
 
 		axios({
 		    url: 'http://y913929d.beget.tech/auth/check',
 		    method: 'get',
-		    headers: {
-		        'sessionId': 'test'
-		    }
+		    headers
 		 })
-		 .then(response => {
-		    console.log(response)
-		 }) 
-		 .catch(err => {
-		    console.log(err);
-		 });
+		 .then(
+        (response) => { 
+          this.setState({menu: response.data});
+        }
+      ) 
+     .catch(
+        (error) => { 
+          this.setState({menu: error.response.data});
+          if (headers) {
+                this.refs.notify.notificationAlert(
+                {
+                    place: 'br',
+                    message: (
+                        <div>
+                            <div>
+                                Ошибка токена
+                            </div>
+                        </div>
+                    ),
+                    type: "info",
+                    icon: "",
+                    autoDismiss: 5
+                }
+              );
+            localStorage.token = '';
+          }
+        }
+      )
 
   }
 
@@ -91,24 +90,53 @@ class App extends Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/" active={localStorage.section === 'auth'} onClick={() => this.selectSection('auth')}>Авторизция</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" active={localStorage.section === 'reg'}  onClick={() => this.selectSection('reg')}>Регистрация</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" active={localStorage.section === 'profile'} onClick={() => this.selectSection('profile')}>Мой профиль</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" active={localStorage.section === 'top100'} onClick={() => this.selectSection('top100')}>Тор 100</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" active={localStorage.section === 'myList'} onClick={() => this.selectSection('myList')}>Мой список</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" active={localStorage.section === 'list'} onClick={() => this.selectSection('list')}>Полный список</NavLink>
-              </NavItem>
+             
+              { 
+                this.state.menu.login ? 
+                  <NavItem>
+                    <NavLink href="/" active={localStorage.section === 'auth'} onClick={() => this.selectSection('auth')}>Авторизция</NavLink>
+                  </NavItem> : ''
+              }
+
+              { 
+                this.state.menu.reg ? 
+                  <NavItem>
+                    <NavLink href="/" active={localStorage.section === 'reg'}  onClick={() => this.selectSection('reg')}>Регистрация</NavLink>
+                  </NavItem> : ''
+              }
+
+              { 
+                this.state.menu.profile ? 
+                  <NavItem>
+                    <NavLink href="/" active={localStorage.section === 'profile'} onClick={() => this.selectSection('profile')}>Мой профиль</NavLink>
+                  </NavItem> : ''
+              }
+
+                  <NavItem>
+                    <NavLink href="/" active={localStorage.section === 'top100'} onClick={() => this.selectSection('top100')}>Топ 100</NavLink>
+                  </NavItem>
+
+              { 
+                this.state.menu.my_list ?     
+                  <NavItem>
+                    <NavLink href="/" active={localStorage.section === 'myList'} onClick={() => this.selectSection('myList')}>Мой список</NavLink>
+                  </NavItem> : ''
+              }
+
+              { 
+                this.state.menu.list ?   
+                  <NavItem>
+                    <NavLink href="/" active={localStorage.section === 'list'} onClick={() => this.selectSection('list')}>Полный список</NavLink>
+                  </NavItem> : ''
+              }  
+
+              { 
+                !this.state.menu.login ? 
+                  <NavItem>
+                    <NavLink href="/" onClick={() => {}}>Выход</NavLink>
+                  </NavItem> : ''
+              }
+
             </Nav>
           </Collapse>
         </Navbar>
@@ -117,15 +145,15 @@ class App extends Component {
 
 		<Container>
 		
-        	<button onClick={() => this.myFunc()}>Hey</button>
+          <span> { JSON.stringify(this.state.menu) } </span>
         
         	{ localStorage.section === 'main' || !localStorage.section ? <Main /> : '' }
         	{ localStorage.section === 'auth' ? <Auth /> : '' }	
-			{ localStorage.section === 'reg' ? <Reg /> : '' }	
-			{ localStorage.section === 'profile' ? <Profile /> : '' }	
-			{ localStorage.section === 'top100' ? <Top100 /> : '' }
-			{ localStorage.section === 'myList' ? <MyList /> : '' }
-			{ localStorage.section === 'list' ? <List /> : '' }
+			    { localStorage.section === 'reg' ? <Reg /> : '' }	
+			    { localStorage.section === 'profile' ? <Profile /> : '' }	
+			    { localStorage.section === 'top100' ? <Top100 /> : '' }
+			    { localStorage.section === 'myList' ? <MyList /> : '' }
+			    { localStorage.section === 'list' ? <List /> : '' }
 
       	</Container>
 
